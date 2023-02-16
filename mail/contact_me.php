37e22390cdb4db3337ac6d6d4eef2487bfe4e088
gandakisewa <124920581+gandakisewa@users.noreply.github.com>
2023-02-16 20:30:02 +0545

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,53 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+{
 	echo "No arguments Provided!";
 	return false;
-   }
+}
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+
+// Set your Zoho Mail settings
+$smtpHost = 'smtp.zoho.com';
+$smtpUsername = 'YOUR_EMAIL_ADDRESS';
+$smtpPassword = 'YOUR_EMAIL_PASSWORD';
+$smtpPort = 465;
+$smtpSecure = 'ssl';
+
 // Create the email and send the message
-$to = 'contact@gandakinirmansewa.org.np'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@gandakinirmansew.org.np\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+require_once "vendor/autoload.php"; // Include PHPMailer library
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = $smtpHost;                              // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = $smtpUsername;                          // SMTP username
+    $mail->Password   = $smtpPassword;                          // SMTP password
+    $mail->SMTPSecure = $smtpSecure;                             // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = $smtpPort;                               // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom($email_address, $name);                      // Sender email and name
+    $mail->addAddress('contact@gandakinirmansewa.org.np');       // Recipient email address
+    $mail->addReplyTo($email_address, $name);                    // Reply-to email and name
+
+    // Content
+    $mail->isHTML(true);                                        // Set email format to HTML
+    $mail->Subject = "Website Contact Form:  $name";
+    $mail->Body    = "You have received a new message from your website contact form.<br><br>".
+                     "Here are the details:<br><br>Name: $name<br>Email: $email_address<br>Message:<br>$message";
+
+    $mail->send();
+    return true;
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    return false;
+}
 ?>
